1b286120b80c310be0d44750cac210b27f979dc4
VMoM <ukfg.poubelle@gmail.com>
2020-12-30 17:16:00 +0100

    Adaptation to the new view (TitleView)

--- modules/settings.py
+++ modules/settings.py
@@ -7,30 +7,39 @@ GRID_HEIGHT = 22
 
 
 ###############################################################
-############################ VIEW #############################
+######################### GAME VIEW ###########################
 ###############################################################
-VIEW_GRID_BEGIN_X = 1
-VIEW_GRID_BEGIN_Y = 1
-VIEW_GRID_WIDTH = GRID_WIDTH*2 + 2 + 1  # grille*2 + bordures + l'espace pour pouvoir tout print
+GAME_VIEW_GRID_BEGIN_X = 1
+GAME_VIEW_GRID_BEGIN_Y = 1
+GAME_VIEW_GRID_WIDTH = GRID_WIDTH * 2 + 2 + 1  # grille*2 + bordures + l'espace pour pouvoir tout print
 # (*2 car les tétrominos font 2 de large)
-VIEW_GRID_HEIGHT = GRID_HEIGHT + 2  # grille + bordures
+GAME_VIEW_GRID_HEIGHT = GRID_HEIGHT + 2  # grille + bordures
 
-VIEW_NEXT_BEGIN_X = VIEW_GRID_BEGIN_X + VIEW_GRID_WIDTH + 2
-VIEW_NEXT_BEGIN_Y = 1
-VIEW_NEXT_WIDTH = 4*2 + 2  # grille*2 + bordures (*2 car les tétrominos font 2 de large)
-VIEW_NEXT_HEIGHT = 2 + 4 + 1  # grille + bordures + l'espace pour pouvoir tout print
+GAME_VIEW_NEXT_BEGIN_X = GAME_VIEW_GRID_BEGIN_X + GAME_VIEW_GRID_WIDTH + 2
+GAME_VIEW_NEXT_BEGIN_Y = 1
+GAME_VIEW_NEXT_WIDTH = 4 * 2 + 2  # grille*2 + bordures (*2 car les tétrominos font 2 de large)
+GAME_VIEW_NEXT_HEIGHT = 2 + 4 + 1  # grille + bordures + l'espace pour pouvoir tout print
 
-VIEW_STORED_BEGIN_X = VIEW_NEXT_BEGIN_X + VIEW_NEXT_WIDTH + 2
-VIEW_STORED_BEGIN_Y = 1
-VIEW_STORED_WIDTH = 4*2 + 2  # grille*2 + bordures (*2 car les tétrominos font 2 de large)
-VIEW_STORED_HEIGHT = 2 + 4 + 1  # grille + bordures + l'espace pour pouvoir tout print
+GAME_VIEW_STORED_BEGIN_X = GAME_VIEW_NEXT_BEGIN_X + GAME_VIEW_NEXT_WIDTH + 2
+GAME_VIEW_STORED_BEGIN_Y = 1
+GAME_VIEW_STORED_WIDTH = 4 * 2 + 2  # grille*2 + bordures (*2 car les tétrominos font 2 de large)
+GAME_VIEW_STORED_HEIGHT = 2 + 4 + 1  # grille + bordures + l'espace pour pouvoir tout print
 
-VIEW_STATISTICS_BEGIN_X = VIEW_GRID_BEGIN_X + VIEW_GRID_WIDTH + 2
-VIEW_STATISTICS_BEGIN_Y = VIEW_NEXT_BEGIN_Y + VIEW_NEXT_HEIGHT
-VIEW_STATISTICS_WIDTH = 18 + 2 + 2  # Statistiques + bordures
-VIEW_STATISTICS_HEIGHT = 4 + 2 + 1  # grille + bordures + l'espace pour pouvoir tout print
+GAME_VIEW_STATISTICS_BEGIN_X = GAME_VIEW_GRID_BEGIN_X + GAME_VIEW_GRID_WIDTH + 2
+GAME_VIEW_STATISTICS_BEGIN_Y = GAME_VIEW_NEXT_BEGIN_Y + GAME_VIEW_NEXT_HEIGHT
+GAME_VIEW_STATISTICS_WIDTH = 18 + 2 + 2  # Statistiques + bordures
+GAME_VIEW_STATISTICS_HEIGHT = 4 + 2 + 1  # grille + bordures + l'espace pour pouvoir tout print
 
-VIEW_KEYBINDS_BEGIN_X = VIEW_GRID_BEGIN_X + VIEW_GRID_WIDTH + 2
-VIEW_KEYBINDS_BEGIN_Y = VIEW_STATISTICS_BEGIN_Y + VIEW_STATISTICS_HEIGHT
-VIEW_KEYBINDS_WIDTH = 18 + 2 + 2  # Keymaps + bordures
-VIEW_KEYBINDS_HEIGHT = 8 + 2 + 1  # Keymaps + bordures + l'espace pour pouvoir tout print
+GAME_VIEW_KEYBINDS_BEGIN_X = GAME_VIEW_GRID_BEGIN_X + GAME_VIEW_GRID_WIDTH + 2
+GAME_VIEW_KEYBINDS_BEGIN_Y = GAME_VIEW_STATISTICS_BEGIN_Y + GAME_VIEW_STATISTICS_HEIGHT
+GAME_VIEW_KEYBINDS_WIDTH = 18 + 2 + 2  # Keymaps + bordures
+GAME_VIEW_KEYBINDS_HEIGHT = 8 + 2 + 1  # Keymaps + bordures + l'espace pour pouvoir tout print
+
+
+###############################################################
+######################## TITLE VIEW ###########################
+###############################################################
+TITLE_VIEW_LOGO_BEGIN_X = 3
+TITLE_VIEW_LOGO_BEGIN_Y = 2
+TITLE_VIEW_LOGO_WIDTH = 26
+TITLE_VIEW_LOGO_HEIGHT = 14
