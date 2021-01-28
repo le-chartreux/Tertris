b7bcbed29f56041de2fd66911c34553aded6b1f5
VMoM <ukfg.poubelle@gmail.com>
2021-01-28 22:55:53 +0100

    Adding a printing utility

--- modules/debug_utilities.py
+++ modules/debug_utilities.py
@@ -2,10 +2,16 @@
 # INFORMATIONS SUR CE PACKAGE :
 # -----------------------------
 # UTILITÉ DE SON CONTENU :
-# Définir des fonctions utiles pour debug, puisque Curses ne permet plus de print()
+# Définir des fonctions utiles pour debug
 # ==========================================================
 
 def print_in_output_dot_txt(text: str):
+    # =============================
+    # INFORMATIONS :
+    # -----------------------------
+    # UTILITÉ :
+    # Ajoute le texte dans le fichier "output.txt", utile puisque curses ne permet pas de print de message
+    # =============================
     output_dot_txt = open("output.txt", "a")
     output_dot_txt.write(text + '\n')
     output_dot_txt.close()
