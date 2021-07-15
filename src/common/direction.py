9dbd91d739f45a630d82cc5c57368a7970a4f1fd
VMoM <vmom.divers@gmail.com>
2021-07-16 01:11:04 +0200

    adding axe-variation getters for Direction

--- src/common/direction.py
+++ src/common/direction.py
@@ -10,3 +10,25 @@ class Direction(enum.Enum):
     RIGHT = enum.auto()
     UP = enum.auto()
     DOWN = enum.auto()
+
+    def get_x_variation(self) -> int:
+        """
+        :return: how the horizontal position of the object will change by going to this direction
+        """
+        if self == Direction.LEFT:
+            return -1
+        elif self == Direction.RIGHT:
+            return 1
+        else:
+            return 0
+
+    def get_y_variation(self) -> int:
+        """
+        :return: how the vertical position of the object will change by going to this direction
+        """
+        if self == Direction.UP:
+            return -1
+        elif self == Direction.LEFT:
+            return 1
+        else:
+            return 0
