bd7aa74e925af8f54dcf6ee4a09deb56a7f444f7
VMoM <vmom.divers@gmail.com>
2021-07-16 01:11:37 +0200

    using new axe-variation getters for Direction

--- src/model/active_tetromino.py
+++ src/model/active_tetromino.py
@@ -1,16 +1,20 @@
 """
 File that contains the declaration of the ActiveTetromino class
 """
-import model.shape as m_shape
 import common.direction as m_direction
 import common.rotation as m_rotation
+import common.tetromino_type as m_tetromino_type
+
+import model.shape as m_shape
+import model.utils as m_utils
 
 
 class ActiveTetromino:
-    def __init__(self, shape: m_shape.Shape):
+    def __init__(self, tetromino_type: m_tetromino_type.TetrominoType):
         self._x = 0
         self._y = 0
-        self._shape = shape
+        self._type = tetromino_type
+        self._shape = m_utils.get_tetromino_shape(self._type)
 
     # GETTER
     def get_x(self) -> int:
@@ -22,27 +26,24 @@ class ActiveTetromino:
     def get_shape(self) -> m_shape.Shape:
         return self._shape
 
+    def get_tetromino_type(self) -> m_tetromino_type.TetrominoType:
+        return self._type
+
     # OTHER METHODS
     def move(self, direction: m_direction.Direction) -> None:
-        if direction == m_direction.Direction.DOWN:
-            self._y += 1
-        elif direction == m_direction.Direction.UP:
-            self._y -= 1
-        elif direction == m_direction.Direction.LEFT:
-            self._x -= 1
-        elif direction == m_direction.Direction.RIGHT:
-            self._x += 1
-        else:
-            if isinstance(direction, m_direction.Direction):
-                raise ValueError(
-                    "Error: impossible to move the active tetromino: invalid direction (%s)" % direction.name
-                )
-            else:
-                raise TypeError(
-                    "Error: impossible to move the active tetromino: direction must be a Direction but a %s is given"
-                    % type(direction).name
-                )
+        """
+        Move the tetromino by one box according to the asked direction
+
+        :param direction: the direction the tetromino has to move to
+        """
+        self._x += direction.get_x_variation()
+        self._y += direction.get_y_variation()
 
     def rotate(self, rotation: m_rotation.Rotation) -> None:
+        """
+        Move the tetromino by one box according to the asked rotation
+
+        :param rotation: the rotation the tetromino has to move to
+        """
         # TODO
         pass
