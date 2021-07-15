679860f1c5ae7c78f2c6c93c77614aed977ecc1c
VMoM <vmom.divers@gmail.com>
2021-07-16 01:12:09 +0200

    adding is_line_full and drop_lines_upper methods for Grid

--- src/model/grid.py
+++ src/model/grid.py
@@ -2,7 +2,6 @@
 File that contains the declaration of the Shape class
 """
 
-import typing
 import model.active_tetromino as m_active_tetromino
 import model.shape as m_shape
 
@@ -30,3 +29,29 @@ class Grid:
     # SETTERS
     def set_shape(self, new_shape: m_shape.Shape) -> None:
         self.shape = new_shape
+
+    def is_line_full(self, line_number: int) -> bool:
+        """
+        :return: if the asked line is full (no empty boxes)
+        """
+        column_number = 0
+        while column_number <= self.get_shape().get_width() and self.shape.is_occupied(column_number, line_number):
+            column_number += 1
+        return column_number == self.get_shape().get_width()
+
+    def drop_lines_upper(self, line_number: int) -> None:
+        """
+        Replaces each line higher than <line_number> by it's higher.
+        The first line of the grid will become empty.
+        The line <line_number> is included.
+
+        :param line_number: number of the first line to replace by it's higher
+        """
+        # replaces each line higher than <line_number> by it's higher
+        line = line_number
+        while line != 0:
+            self.shape.get_boxes()[line][:] = self.shape.get_boxes()[line - 1][:]
+            line -= 1
+        # We put None on the higher line
+        for column in range(self.shape.get_width()):
+            self.shape.get_boxes()[0][column] = None
