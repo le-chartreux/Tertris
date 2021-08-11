a4436e9cbf7175db054bef327a2c09194667395a
VMoM <vmom.divers@gmail.com>
2021-08-11 12:48:52 +0200

    better test.sh (can now use -v)

--- test.sh
+++ test.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 PATH_TO_DOT="$(dirname "$(readlink -f "$0")")"
 
-python3 -m unittest discover "$PATH_TO_DOT/src" -v
+if [ $# = 1 ] && [ "$1" = "-v" ]
+then
+  python3 -m unittest discover "$PATH_TO_DOT/src" -v
+else
+  python3 -m unittest discover "$PATH_TO_DOT/src"
+fi
