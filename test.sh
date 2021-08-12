8607a765b34e3f80d7dfcf74a4c56ba760ef53ee
VMoM <vmom.divers@gmail.com>
2021-08-12 23:09:29 +0200

    better test.sh (follows conventions)

--- test.sh
+++ test.sh
@@ -2,12 +2,12 @@
 PATH_TO_DOT="$(dirname "$(readlink -f "$0")")"
 CLASSIC_COMMAND="python3 -m unittest discover "$PATH_TO_DOT/src""
 
-if [[ "$@" =~ "-v" ]]
+if [[ "$*" =~ "-v" ]]
 then
   CLASSIC_COMMAND="$CLASSIC_COMMAND -v"
 fi
 
-if [[ "$@" =~ "-s" ]]
+if [[ "$*" =~ "-s" ]]
 then
   CLASSIC_COMMAND="SLOW_TESTS=1 $CLASSIC_COMMAND"
 fi
